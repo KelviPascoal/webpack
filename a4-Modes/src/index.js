369e1c84ad75fdd5380beedab478be1f30ca97ce
kelvi <kelvi_ugo@hotmail.com>
2022-05-15 18:41:47 -0300

    feat: aula de como configurar modes

---
+++ a4-Modes/src/index.js
@@ -0,0 +1,9 @@
+import { calc } from './calc'
+
+console.log('testeeeeeeeeeeeee')
+console.log('respeita o pai')
+
+const result = calc(2, 5)
+
+console.log(result)
+
---
+++ a4-Modes/webpack.dev.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    mode: 'development',
+    devServer: {
+       static: {
+           directory: path.resolve(__dirname, './dist'),
+       },
+       devMiddleware: {
+           index: false
+       }
+    },
+    plugins: [
+        new TerserPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'Modes'
+        })
+    ],
+
+}
