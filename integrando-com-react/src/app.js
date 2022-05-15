7eb1a6182a84b15faebafc86e22021fac8048627
kelvi <kelvi_ugo@hotmail.com>
2022-05-15 18:43:18 -0300

    feat: criando projeto react com webpack

---
+++ integrando-com-react/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => {
+        return (
+            <div>
+                <h1>Meu projeto em react e webpack</h1>
+                <p>Integrado com sucesso!</p>
+            </div>
+        )
+    }
